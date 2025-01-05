5243e43f0ec2c06c42e77826583952394bdb3421
sujita <sujitaregmi189@gmail.com>
2025-01-05 21:25:23 +0545

    Refactor code

--- script.js
+++ script.js
@@ -7,29 +7,10 @@ let currentUnit ="";
 let hourlyorWeek ="Week";
 
 function getDateTime() {
-let now =new Date(),
-hour = now.getHours(),
-minute = now.getMinutes();
-
-
-let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-
-];
-hour = hour% 12;
-if(hour<10){
-    hour = "0"+ hour;
-}
-if (minute < 10) {
-    minute = "0"+ minute;
-}
-let dayString = days[ now.getDay()];
+let now = moment(), 
+hour = now.format("hh"), 
+minute = now.format("mm"),
+dayString = now.format("dddd");
 return `${ dayString}, ${ hour}:${ minute}`;
 }
 date.innerText = getDateTime();
@@ -37,16 +18,40 @@ setInterval(( ) => {
     date.innerText = getDateTime();
 }, 1000 );
   function getPublicIp(){
-    fetch("https://mylocation.org/",{
+    fetch("https://ipapi.co/json",{
     method : "GET",
   })
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    currentCity = data. currentCity;
+    currentCity = data.city;
+    const lat= data.latitude;
+    const lon = data.longitude;
+    getWeatherData(lat,lon);
   });
 }
-getPublicIp();
-function getWeatherData(city, unit, hourlyorWeek) {
-  fetch("")
+//function getWeatherData(city, unit, hourlyorWeek) {       for city
+ function getWeatherData(lat , lon, unit ="metric") {      //for own location
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={85c5de0fdba5cff5a4a312988bbdcf0f&units=$unit}",{
+    method : "GET",
+  })
+  .then((response) => response.json() )
+.then((data)=> {
+  console.log(data);
+  updateWeatherData(data);
+})
+ }
+ function updateWeatherData(data) {
+  const tempElement = document.getElementById("temp");
+  const conditionElement = document.getElementById("condition");
+  const locationElement = document.querySelector(".location-text p");
+
+
+  const temperature = data.main.temp;
+  tempElement.innerText = Math.round(temperature);
+
+  const weatherCondition = data.weather[0].description;
+  conditionElement.innerText = weatherCondition;
+  locationElement.innerText = data.name;
 }
+getPublicIp();
